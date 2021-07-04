1ec172480407e5840f24406a804340a478ca5447
Rav <ravenrnt@gmx.de>
2021-07-04 18:43:12 +0200

    Update Auctionator.lua

--- Skins/AddOns/Retail/Auctionator.lua
+++ Skins/AddOns/Retail/Auctionator.lua
@@ -252,6 +252,7 @@ end
 local function SkinOptions()
 	local options = {
 		_G.AuctionatorConfigBasicOptionsFrame,
+		_G.AuctionatorConfigQuantitiesFrame,
 		_G.AuctionatorConfigTooltipsFrame,
 		_G.AuctionatorConfigShoppingFrame,
 		_G.AuctionatorConfigSellingFrame,
